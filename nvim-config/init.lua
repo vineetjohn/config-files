765f7c5cdf7623776901eb98fdc0361aa39aecbe
Vineet John <iamvineetjohn@gmail.com>
2025-04-12 16:08:54 -0700

    nvim: add config files

---
+++ nvim-config/init.lua
@@ -0,0 +1,14 @@
+require("config.lazy")
+
+vim.opt.compatible = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.cursorbind = true
+vim.opt.scrollbind = true
+vim.opt.cursorline = true
+
+vim.cmd('colorscheme github_light_high_contrast')
+vim.cmd('syntax on')
